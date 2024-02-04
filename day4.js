39c89e2035584a0a2fb0db2a23e5bfe6129d4110
akshaykushwaha <akshaykumar191200@gmail.com>
2024-02-04 21:29:09 +0530

    day 4 of scaler node js challenge

---
+++ day4.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+function resolvePath(relativePath) {
+    console.log(`Resolved Path: ${path.resolve(relativePath)}`);
+}
+
+resolvePath('project/folder/file.txt');
+// Expected Output: Resolved Path: /Users/username/project/folder/file.txt
+
+resolvePath('nonexistent-folder/file.txt');
+// Expected Output: Resolved Path: /Users/username/nonexistent-folder/file.txt
