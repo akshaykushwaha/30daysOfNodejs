5fdb054d257871778b1eff32e077b3e382cd90ca
akshaykushwaha <akshaykumar191200@gmail.com>
2024-02-10 19:28:30 +0530

    day 6 of nodejs challenge

---
+++ day6.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express()
+
+/**
+ * Handles GET requests to "/greet" endpoint
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+*/
+function greetHandler(req, res) {
+    res.send(`Hello, ${req.query.name ?? 'Guest'}!`)
+}
+
+app.get('/greet', greetHandler);
+
+app.listen(3000, () => console.log("Port is running on 3000"))
